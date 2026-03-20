979590b5530bc99f6d6fd29f0012ef88e7f50e2e
nika todua <alchimikos@gmail.com>
2026-03-20 11:31:32 +0400

    update and add project component

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Header } from './header/header';
 import { Home } from './page/home/home';
+import { Projects } from './page/projects/projects';
 
 @NgModule({
-  declarations: [App, Header, Home],
+  declarations: [App, Header, Home, Projects],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay())],
   bootstrap: [App],
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './page/home/home';
+import { Projects } from './page/projects/projects';
 
 const routes: Routes = [
-  {path: '', component:Home, title:"Todwood - საუნების ხელოვნება"}
+  {path: '', component:Home, title:"Todwood - საუნების ხელოვნება"},
+  {path: 'projects', component:Projects, title:"ჩვენი ნამუშევრები"}
 ];
 
 @NgModule({
