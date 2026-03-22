b43be2343ec8668c8ea5b1d49cec92130d0a14ae
nika todua <alchimikos@gmail.com>
2026-03-22 22:34:28 +0400

    Add project and services page content in Georgian; update footer and contact details

--- src/app/page/projects/projects.ts
+++ src/app/page/projects/projects.ts
@@ -10,28 +10,7 @@ export class Projects {
 
   projectsArray:any = [
     {
-      img:'https://images.unsplash.com/photo-1724301930658-2f21ae702d07?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxsdXh1cnklMjBzYXVuYSUyMGludGVyaW9yJTIwY2VkYXIlMjB3b29kfGVufDF8fHx8MTc3Mzg1NDA3NHww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-      category:'საცხოვრებელი',
-      title:'ძვირადღირებული სახლის საუნა',
-      desc:'პრემიუმ კლასის კედრის ინტერიერი ინდივიდუალური განათებით და დეკორატიული დიზაინით.',
-      location:'ტაჰოს ტბა, კალიფორნია',
-    },
-    {
-      img:'https://images.unsplash.com/photo-1724301930658-2f21ae702d07?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxsdXh1cnklMjBzYXVuYSUyMGludGVyaW9yJTIwY2VkYXIlMjB3b29kfGVufDF8fHx8MTc3Mzg1NDA3NHww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-      category:'საცხოვრებელი',
-      title:'ძვირადღირებული სახლის საუნა',
-      desc:'პრემიუმ კლასის კედრის ინტერიერი ინდივიდუალური განათებით და დეკორატიული დიზაინით.',
-      location:'ტაჰოს ტბა, კალიფორნია',
-    },
-    {
-      img:'https://images.unsplash.com/photo-1724301930658-2f21ae702d07?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxsdXh1cnklMjBzYXVuYSUyMGludGVyaW9yJTIwY2VkYXIlMjB3b29kfGVufDF8fHx8MTc3Mzg1NDA3NHww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-      category:'საცხოვრებელი',
-      title:'ძვირადღირებული სახლის საუნა',
-      desc:'პრემიუმ კლასის კედრის ინტერიერი ინდივიდუალური განათებით და დეკორატიული დიზაინით.',
-      location:'ტაჰოს ტბა, კალიფორნია',
-    },
-    {
-      img:'https://images.unsplash.com/photo-1724301930658-2f21ae702d07?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxsdXh1cnklMjBzYXVuYSUyMGludGVyaW9yJTIwY2VkYXIlMjB3b29kfGVufDF8fHx8MTc3Mzg1NDA3NHww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+      img:'https://images.unsplash.com/photo-1724301930658-2f21ae702d07',
       category:'საცხოვრებელი',
       title:'ძვირადღირებული სახლის საუნა',
       desc:'პრემიუმ კლასის კედრის ინტერიერი ინდივიდუალური განათებით და დეკორატიული დიზაინით.',
